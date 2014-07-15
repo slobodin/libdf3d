55d739175be48291e99cace73c020be7c6044098
flaming0 <epiforce57@gmail.com>
2014-07-15 18:05:29 +0400

    update librocket android binaries

--- third-party/Android.mk
+++ third-party/Android.mk
@@ -1,16 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
 
-# include $(CLEAR_VARS)
-# LOCAL_MODULE:= librocket
-# LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/librocket.so
-# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
-# include $(PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE:= librocket
+LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/librocket.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
+include $(PREBUILT_SHARED_LIBRARY)
 
-# include $(CLEAR_VARS)
-# LOCAL_MODULE:= libpyrocketcore
-# LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/libpyrocketcore.so
-# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
-# include $(PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libpyrocketcore
+LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/libpyrocketcore.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
+include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libfreetype
@@ -36,14 +36,14 @@ LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/libspark.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
 include $(PREBUILT_SHARED_LIBRARY)
 
-# include $(CLEAR_VARS)
-# LOCAL_MODULE:= libogg
-# LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/libogg.so
-# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
-# include $(PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libogg
+LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/libogg.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
+include $(PREBUILT_SHARED_LIBRARY)
 
-# include $(CLEAR_VARS)
-# LOCAL_MODULE:= libvorbis
-# LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/libvorbis.so
-# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
-# include $(PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libvorbis
+LOCAL_SRC_FILES:= lib/android/$(TARGET_ARCH_ABI)/libvorbis.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
+include $(PREBUILT_SHARED_LIBRARY)
