95417c3193a42bccad329ea25ae967526bbd289f
flamingo <epiforce57@gmail.com>
2014-10-25 20:02:43 +0400

    Fix.

--- libdf3d/gui/cegui_impl/CeguiViewportTargetImpl.cpp
+++ libdf3d/gui/cegui_impl/CeguiViewportTargetImpl.cpp
@@ -1,17 +1,11 @@
 #include "df3d_pch.h"
 #include "CeguiViewportTargetImpl.h"
 
-// TEST!!!
-#include <render/OpenGLCommon.h>
-
 namespace df3d { namespace gui { namespace cegui_impl {
 
 CeguiViewportTargetImpl::CeguiViewportTargetImpl(CeguiRendererImpl &owner, int width, int height)
     : CeguiRenderTargetImpl<CEGUI::RenderTarget>(owner)
 {
-    GLint vp[4];
-    glGetIntegerv(GL_VIEWPORT, vp);
-
     CEGUI::Rectf initArea(0.0f, 0.0f, (float)width, (float)height);
     setArea(initArea);
 }
