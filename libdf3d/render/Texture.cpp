275ce50b77d7928ce821f39769e165c137d36026
flamingo <epiforce57@gmail.com>
2014-12-14 15:15:32 +0300

    Fix.

--- libdf3d/render/Texture.cpp
+++ libdf3d/render/Texture.cpp
@@ -166,8 +166,7 @@ bool Texture::createGLTexture()
         glPixelFormat = GL_RGBA;
         break;
     case Image::Format::GRAYSCALE:
-        return false;
-        //glPixelFormat = GL_LUMINANCE;   // FIXME: is it valid on ES?
+        glPixelFormat = GL_LUMINANCE;   // FIXME: is it valid on ES?
         break;
     default:
         base::glog << "Invalid GL texture pixel format" << base::logwarn;
