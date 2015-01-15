f3986f83d4596264bb09bb211413d4e5e294b548
flamingo <epiforce57@gmail.com>
2015-01-15 23:18:47 +0300

    Add forcing for no alignment.

--- third-party/include/bullet/LinearMath/btScalar.h
+++ third-party/include/bullet/LinearMath/btScalar.h
@@ -42,7 +42,7 @@ inline int	btGetVersion()
 
 #ifdef _WIN32
 
-		#if defined(__MINGW32__) || defined(__CYGWIN__) || (defined (_MSC_VER) && _MSC_VER < 1300)
+		#if defined(__MINGW32__) || defined(__CYGWIN__) || (defined (_MSC_VER) && _MSC_VER < 1300) || defined(BULLET_FORCE_NO_ALIGNMENT)
 
 			#define SIMD_FORCE_INLINE inline
 			#define ATTRIBUTE_ALIGNED16(a) a
