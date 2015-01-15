e7cac85a742ae0ad808f7364d5998429c0579444
flamingo <epiforce57@gmail.com>
2015-01-15 23:31:26 +0300

    Add stub for wp platform.

---
+++ libdf3d/platform/windows_phone/WindowsPhoneApplication.cpp
@@ -0,0 +1,31 @@
+#include "df3d_pch.h"
+#include "WindowsPhoneApplication.h"
+
+namespace df3d { namespace platform {
+
+WindowsPhoneApplication::WindowsPhoneApplication(const AppInitParams &params)
+{
+
+}
+
+WindowsPhoneApplication::~WindowsPhoneApplication()
+{
+
+}
+
+bool WindowsPhoneApplication::pollEvents()
+{
+
+}
+
+void WindowsPhoneApplication::swapBuffers()
+{
+
+}
+
+void WindowsPhoneApplication::setTitle(const char *title)
+{
+
+}
+
+} }
