81ca1187d699381bc50549f1acfbf6c76b93e27b
flamingo <epiforce57@gmail.com>
2015-01-19 00:58:44 +0300

    Do not create application for wp. Shitty solution.

--- libdf3d/platform/Application.cpp
+++ libdf3d/platform/Application.cpp
@@ -4,7 +4,7 @@
 #if defined(DF3D_WINDOWS)
 #include <platform/windows/WindowsApplication.h>
 #elif defined(DF3D_WINDOWS_PHONE)
-#include <platform/windows_phone/WindowsPhoneApplication.h>
+
 #else
 #error "Unsupported platform"
 #endif
@@ -25,7 +25,8 @@ Application *Application::create(const AppInitParams &params)
 #if defined(DF3D_WINDOWS)
     return new WindowsApplication(params);
 #elif defined(DF3D_WINDOWS_PHONE)
-    return new WindowsPhoneApplication(params);
+    assert(false);
+    return nullptr;
 #else
     return nullptr;
 #endif
