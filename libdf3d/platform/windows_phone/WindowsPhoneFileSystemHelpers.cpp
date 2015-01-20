8f80ab2185d53f3823e69a01219b33105c5a4c1e
flamingo <epiforce57@gmail.com>
2015-01-20 21:48:12 +0300

    Filesystem helpers for wp.

--- libdf3d/platform/windows_phone/WindowsPhoneFileSystemHelpers.cpp
+++ libdf3d/platform/windows_phone/WindowsPhoneFileSystemHelpers.cpp
@@ -1,2 +1,19 @@
 #include "df3d_pch.h"
 #include "WindowsPhoneFileSystemHelpers.h"
+
+namespace df3d { namespace platform {
+
+bool FileSystemHelpers::isPathAbsolute(const std::string &path)
+{
+    if (path.size() < 2)
+        return false;
+
+    return path[1] == ':' && ((path[0] >= 'a' && path[0] <= 'z') || (path[0] >= 'A' || path[0] <= 'Z'));
+}
+
+bool FileSystemHelpers::pathExists(const std::string &path)
+{
+    return false;
+}
+
+} }
