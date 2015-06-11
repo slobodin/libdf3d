94b915c3fb7e04340608a6a660d813ddc1758d11
nikolays <nikolays@zeptolab.com>
2015-06-11 19:38:40 +0300

    work

--- libdf3d/platform/Storage.h
+++ libdf3d/platform/Storage.h
@@ -4,15 +4,36 @@ namespace df3d { namespace platform {
 
 class DF3D_DLL Storage : boost::noncopyable
 {
+protected:
+    using Entry = boost::variant<int32_t,
+                                 int64_t,
+                                 float,
+                                 double,
+                                 bool,
+                                 std::string>;
+
+    std::unordered_map<std::string, Entry> m_entries;
+
+    Storage() { }
+    virtual ~Storage() { }
+
 public:
     static Storage *getInstance();
 
     template<typename T>
     T &operator[](const char *key);
 
-protected:
-    Storage() { }
-    virtual ~Storage() { }
+    template<typename T>
+    const T &operator[](const char *key) const;
+
+    bool contains(const char *key) const;
+
+    bool erase(const char *key);
+    void clear();
+
+    void save();
+
+
 };
 
 } }
