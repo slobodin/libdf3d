9608fce3a1ad9a1b6b43cb4a6d9e859d9887a41f
nikolays <nikolays@zeptolab.com>
2015-06-11 18:35:54 +0300

    t

--- libdf3d/platform/Storage.h
+++ libdf3d/platform/Storage.h
@@ -1,2 +1,18 @@
 #pragma once
 
+namespace df3d { namespace platform {
+
+class DF3D_DLL Storage : boost::noncopyable
+{
+public:
+    static Storage *getInstance();
+
+    template<typename T>
+    T &operator[](const char *key);
+
+protected:
+    Storage() { }
+    virtual ~Storage() { }
+};
+
+} }
