51afb2a573fc791116cc10279adc68742e9f9da4
flamingo <epiforce57@gmail.com>
2015-06-16 23:59:11 +0300

    Linux.

--- cmake/libdf3d.cmake
+++ cmake/libdf3d.cmake
@@ -17,6 +17,8 @@ elseif (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
         set(LINUX true)
         message(STATUS "Platform - Linux")
     endif()
+
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
 elseif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     if (IOS)
 
