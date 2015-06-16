59a6f7c97c3c2266078056769e70fee81a2b3524
flamingo <epiforce57@gmail.com>
2015-06-16 21:50:47 +0300

    Add windows phone storage.

---
+++ libdf3d/platform/windows_phone/Storage.cpp
@@ -0,0 +1,52 @@
+#include "df3d_pch.h"
+#include "../Storage.h"
+
+#include <utils/JsonHelpers.h>
+
+namespace df3d { namespace platform { 
+
+class WindowsRTStorage : public Storage
+{
+public:
+    WindowsRTStorage(const char *filename)
+        : Storage(filename)
+    {
+        auto root = utils::jsonLoadFromFile(filename);
+        if (!!root)
+        {
+            df3d::base::glog << "Failed to init Storage. Can not open file" << df3d::base::logwarn;
+            return;
+        }
+
+        for (auto it = root.begin(); it != root.end(); it++)
+        {
+            const auto key = it.key().asString();
+            Storage::Entry val;
+
+            if (it->isBool())
+                val = it->asBool();
+            else if (it->isInt())
+                val = it->isInt();
+            else if (it->isInt64())
+                val = it->isInt64();
+            else if (it->isDouble())
+                val = it->asDouble();
+            else if (it->isString())
+                val = it->asString();
+
+            m_entries[key] = val;
+        }
+    }
+
+    virtual void save() override
+    {
+
+    }
+};
+
+Storage *Storage::create(const char *filename)
+{
+    return new WindowsRTStorage(filename);
+}
+
+} }
