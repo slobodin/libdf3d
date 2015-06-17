033873735aae94e37d41d432527debac74c4dbeb
flamingo <epiforce57@gmail.com>
2015-06-18 00:20:20 +0300

    Linux app.

--- libdf3d/platform/linux/LinuxApplication.cpp
+++ libdf3d/platform/linux/LinuxApplication.cpp
@@ -3,5 +3,60 @@
 
 namespace df3d { namespace platform {
 
+struct LinuxApplication::Impl
+{
+
+};
+
+LinuxApplication::LinuxApplication(const AppInitParams &params, AppDelegate *appDelegate)
+    : Application(params, appDelegate),
+    m_pImpl(make_unique<Impl>())
+{
+
+}
+
+LinuxApplication::~LinuxApplication()
+{
+
+}
+
+void LinuxApplication::run()
+{
+    using namespace std::chrono;
+    MSG msg;
+
+    TimePoint currtime, prevtime;
+    currtime = prevtime = system_clock::now();
+
+//    while (!m_pImpl->window->quitRequested())
+//    {
+//        // FIXME:
+//        // Maybe queue messages?
+//        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+//        {
+//            TranslateMessage(&msg);
+//            DispatchMessage(&msg);
+//        }
+//        else
+//        {
+//            currtime = system_clock::now();
+
+//            m_appDelegate->onAppUpdate(IntervalBetween(currtime, prevtime));
+
+//            m_pImpl->window->swapBuffers();
+
+//            prevtime = currtime;
+//        }
+//    }
+
+    m_appDelegate->onAppEnded();
+
+    m_pImpl.reset();
+}
+
+void LinuxApplication::setTitle(const char *title)
+{
+
+}
 
 } }
