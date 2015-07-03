73b1f9f803d13832904d87d9e496c28a4caeedc6
nikolays <nikolays@zeptolab.com>
2015-07-03 19:26:12 +0300

    Fixing cmakes.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -9,3 +9,27 @@ endif()
 if (CMAKE_BUILD_TYPE STREQUAL "Release")
     add_definitions(-DNDEBUG)
 endif()
+
+if (WINDOWS)
+    add_definitions(-DWIN32 D_WINDOWS -DDF3D_WINDOWS)
+endif()
+
+if (ANDROID)
+    add_definitions(-DDF3D_ANDROID)
+endif()
+
+if (WINDOWS_PHONE)
+    add_definitions(-DDF3D_WINDOWS_PHONE)
+endif()
+
+if (LINUX)
+    add_definitions(-DDF3D_LINUX)
+endif()
+
+if (MACOSX)
+    add_definitions(-DDF3D_MACOSX)
+endif()
+
+if (DF3D_DESKTOP)
+    add_definitions(-DDF3D_DESKTOP)
+endif()
