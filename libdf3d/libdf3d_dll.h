b7458793c272d8e354f9aad522e1fc4c67c18c48
flamingo <epiforce57@gmail.com>
2015-07-06 22:07:52 +0300

    Fixing cmakes.

--- libdf3d/libdf3d_dll.h
+++ libdf3d/libdf3d_dll.h
@@ -1,18 +1,15 @@
 #pragma once
 
-// Force static for now
-#if 0
-#if defined(DF3D_WINDOWS) || defined(DF3D_WINDOWS_PHONE)
-#if defined(LIBDF3D_EXPORTS)
-#define DF3D_DLL __declspec(dllexport)
+#if defined(DF3D_SHARED_LIBRARY)
+    #if defined(DF3D_WINDOWS) || defined(DF3D_WINDOWS_PHONE)
+        #if defined(LIBDF3D_EXPORTS)
+            #define DF3D_DLL __declspec(dllexport)
+        #else
+            #define DF3D_DLL __declspec(dllimport)
+        #endif
+    #else
+        #define DF3D_DLL __attribute__((visibility("default")))
+    #endif
 #else
-#define DF3D_DLL __declspec(dllimport)
+    #define DF3D_DLL
 #endif
-#elif defined(DF3D_ANDROID)
-#define DF3D_DLL __attribute__((visibility("default")))
-#else
-#define DF3D_DLL
-#endif
-#endif
-
-#define DF3D_DLL
