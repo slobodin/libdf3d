c293967ab6a090233f570fd91c1e7cb357278678
flaming0 <epiforce57@gmail.com>
2015-07-15 23:31:07 +0300

    Android touches.

--- libdf3d/platform/android/AndroidBridge.cpp
+++ libdf3d/platform/android/AndroidBridge.cpp
@@ -7,8 +7,28 @@
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
 
-JavaVM *g_JavaVm = nullptr;
-df3d::platform::AppDelegate *g_appDelegate = nullptr;
+struct AndroidTouch
+{
+    df3d::base::TouchEvent touch;
+    bool valid = false;
+};
+
+static const int MAX_TOUCHES = 16;
+
+struct AndroidAppState
+{
+    JavaVM *javaVm = nullptr;
+    df3d::platform::AppDelegate *appDelegate = nullptr;
+
+    AndroidTouch touchesCache[MAX_TOUCHES];
+
+    ~AndroidAppState()
+    {
+        SAFE_DELETE(appDelegate);
+    }
+};
+
+AndroidAppState *g_appState = nullptr;
 
 extern void df3dInitialized();
 
@@ -16,7 +36,9 @@ namespace df3d { namespace platform {
 
 void setupDelegate(df3d::platform::AppDelegate *appDelegate)
 {
-    g_appDelegate = appDelegate;
+    assert(g_appState);
+
+    g_appState->appDelegate = appDelegate;
 
     df3d::base::glog << "App delegate was set up" << df3d::base::logmess;
 }
@@ -30,14 +52,15 @@ void run()
 
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 {
-    g_JavaVm = vm;
-
     JNIEnv *env;
     if (vm->GetEnv((void **)(&env), JNI_VERSION_1_6) != JNI_OK)
     {
         df3d::base::glog << "Failed to get environment" << df3d::base::logcritical;
         return -1;
     }
+
+    g_appState = new AndroidAppState();
+    g_appState->javaVm = vm;
 
     df3d::base::glog << "JNI_OnLoad success" << df3d::base::logmess;
 
@@ -58,7 +81,7 @@ extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_init(JNI
     // Must be initialized by client code!
     if (!g_engineController->initialized())
     {
-        if (!g_appDelegate->onAppStarted(jscreenWidth, jscreenHeight))
+        if (!g_appState->appDelegate->onAppStarted(jscreenWidth, jscreenHeight))
             df3d::base::glog << "Failed to initialize game code." << df3d::base::logcritical;
 
         // TODO:
@@ -73,63 +96,106 @@ extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_init(JNI
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_onResume(JNIEnv* env, jclass cls)
 {
-    g_appDelegate->onAppResumed();
+    g_appState->appDelegate->onAppResumed();
 }
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_onPause(JNIEnv* env, jclass cls)
 {
-    g_appDelegate->onAppPaused();
+    g_appState->appDelegate->onAppPaused();
 }
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_onDestroy(JNIEnv* env, jclass cls)
 {
     df3d::base::glog << "Activity was destroyed" << df3d::base::logmess;
-    g_appDelegate->onAppEnded();
+    g_appState->appDelegate->onAppEnded();
 
-    SAFE_DELETE(g_appDelegate);
+    SAFE_DELETE(g_appState);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_draw(JNIEnv* env, jclass cls, jdouble dt)
 {
-    g_appDelegate->onAppUpdate((float)dt);
+    g_appState->appDelegate->onAppUpdate((float)dt);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_onTouchDown(JNIEnv* env, jclass cls, jint pointerId, jfloat x, jfloat y)
 {
-    df3d::base::TouchEvent ev;
-    ev.x = x;
-    ev.y = y;
-    ev.id = pointerId;
-    ev.state = df3d::base::TouchEvent::State::DOWN;
-    g_appDelegate->onTouchEvent(ev);
+    if (pointerId >= MAX_TOUCHES)
+    {
+        df3d::base::glog << "Touch limit exceeded" << df3d::base::logwarn;
+        return;
+    }
+
+    auto &touch = g_appState->touchesCache[pointerId];
+    touch.valid = true;
+    touch.touch.x = x;
+    touch.touch.y = y;
+    touch.touch.dx = 0;
+    touch.touch.dy = 0;
+    touch.touch.id = pointerId;
+    touch.touch.state = df3d::base::TouchEvent::State::DOWN;
+
+    g_appState->appDelegate->onTouchEvent(touch.touch);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_onTouchUp(JNIEnv* env, jclass cls, jint pointerId, jfloat x, jfloat y)
 {
-    df3d::base::TouchEvent ev;
-    ev.x = x;
-    ev.y = y;
-    ev.id = pointerId;
-    ev.state = df3d::base::TouchEvent::State::UP;
-    g_appDelegate->onTouchEvent(ev);
+    if (pointerId >= MAX_TOUCHES)
+    {
+        df3d::base::glog << "Touch limit exceeded" << df3d::base::logwarn;
+        return;
+    }
+
+    auto &touch = g_appState->touchesCache[pointerId];
+    touch.valid = false;
+    touch.touch.x = x;
+    touch.touch.y = y;
+    touch.touch.dx = 0;
+    touch.touch.dy = 0;
+    touch.touch.id = pointerId;
+    touch.touch.state = df3d::base::TouchEvent::State::UP;
+
+    g_appState->appDelegate->onTouchEvent(touch.touch);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_onTouchMove(JNIEnv* env, jclass cls, jint pointerId, jfloat x, jfloat y)
 {
-    df3d::base::TouchEvent ev;
-    ev.x = x;
-    ev.y = y;
-    ev.id = pointerId;
-    ev.state = df3d::base::TouchEvent::State::MOVING;
-    g_appDelegate->onTouchEvent(ev);
+    if (pointerId >= MAX_TOUCHES)
+    {
+        df3d::base::glog << "Touch limit exceeded" << df3d::base::logwarn;
+        return;
+    }
+
+    auto &touch = g_appState->touchesCache[pointerId];
+    if (touch.valid)
+    {
+        touch.touch.dx = x - touch.touch.x;
+        touch.touch.dy = y - touch.touch.y;
+    }
+
+    touch.touch.x = x;
+    touch.touch.y = y;
+    touch.touch.id = pointerId;
+    touch.touch.state = df3d::base::TouchEvent::State::MOVING;
+
+    g_appState->appDelegate->onTouchEvent(touch.touch);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_org_flaming0_df3d_NativeBindings_onTouchCancel(JNIEnv* env, jclass cls, jint pointerId, jfloat x, jfloat y)
 {
-    df3d::base::TouchEvent ev;
-    ev.x = x;
-    ev.y = y;
-    ev.id = pointerId;
-    ev.state = df3d::base::TouchEvent::State::CANCEL;
-    g_appDelegate->onTouchEvent(ev);
+    if (pointerId >= MAX_TOUCHES)
+    {
+        df3d::base::glog << "Touch limit exceeded" << df3d::base::logwarn;
+        return;
+    }
+
+    auto &touch = g_appState->touchesCache[pointerId];
+    touch.valid = false;
+    touch.touch.x = x;
+    touch.touch.y = y;
+    touch.touch.dx = 0;
+    touch.touch.dy = 0;
+    touch.touch.id = pointerId;
+    touch.touch.state = df3d::base::TouchEvent::State::CANCEL;
+
+    g_appState->appDelegate->onTouchEvent(touch.touch);
 }
