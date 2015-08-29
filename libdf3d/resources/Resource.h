533000684f2e40b8ef6f34600e1fef7820c7bb0c
flamingo <epiforce57@gmail.com>
2015-08-29 19:20:21 +0300

    Work.

--- libdf3d/resources/Resource.h
+++ libdf3d/resources/Resource.h
@@ -7,22 +7,24 @@ class DF3D_DLL Resource : utils::NonCopyable
 protected:
     ResourceGUID m_guid;
 
-    std::atomic<bool> m_initialized;
-    std::atomic<bool> m_resident;
+    std::atomic<bool> m_initialized = false;
+    std::atomic<bool> m_resident = false;
+
+    //! Called by resource decoder. This function should set m_initialized depending on decode result.
+    virtual void onDecoded(bool decodeResult) = 0;
 
 public:
     Resource();
-    virtual ~Resource() { }
+    virtual ~Resource() = default;
 
     void setGUID(const ResourceGUID &guid);
     const ResourceGUID &getGUID() const;
 
     const std::string &getFilePath() const;
 
-    bool isValid() const { return m_initialized; }
+    bool isInitialized() const { return m_initialized; }
     bool isResident() const { return m_resident; }
 
-    void setInitialized(bool initialized = true) { m_initialized = initialized; }
     void setResident(bool resident = true) { m_resident = resident; }
 };
 
