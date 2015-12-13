4e4f39f8da5096ab273fbb41c165eedeed24f2e3
flamingo <epiforce57@gmail.com>
2015-12-14 00:26:14 +0300

    Add boilerplate for transform component.

--- libdf3d/scene/TransformComponentProcessor.cpp
+++ libdf3d/scene/TransformComponentProcessor.cpp
@@ -13,4 +13,129 @@ TransformComponentProcessor::~TransformComponentProcessor()
 
 }
 
+void TransformComponentProcessor::setPosition(ComponentInstance comp, const glm::vec3 &newPosition)
+{
+
+}
+
+void TransformComponentProcessor::setPosition(ComponentInstance comp, float x, float y, float z)
+{
+
+}
+
+void TransformComponentProcessor::setScale(ComponentInstance comp, const glm::vec3 &newScale)
+{
+
+}
+
+void TransformComponentProcessor::setScale(ComponentInstance comp, float x, float y, float z)
+{
+
+}
+
+void TransformComponentProcessor::setScale(ComponentInstance comp, float uniform)
+{
+
+}
+
+void TransformComponentProcessor::setOrientation(ComponentInstance comp, const glm::quat &newOrientation)
+{
+
+}
+
+void TransformComponentProcessor::setOrientation(ComponentInstance comp, const glm::vec3 &eulerAngles, bool rads)
+{
+
+}
+
+void TransformComponentProcessor::setTransformation(ComponentInstance comp, const glm::mat4 &tr)
+{
+
+}
+
+void TransformComponentProcessor::translate(ComponentInstance comp, const glm::vec3 &v)
+{
+
+}
+
+void TransformComponentProcessor::translate(ComponentInstance comp, float x, float y, float z)
+{
+
+}
+
+void TransformComponentProcessor::scale(ComponentInstance comp, const glm::vec3 &v)
+{
+
+}
+
+void TransformComponentProcessor::scale(ComponentInstance comp, float x, float y, float z)
+{
+
+}
+
+void TransformComponentProcessor::scale(ComponentInstance comp, float uniform)
+{
+
+}
+
+void TransformComponentProcessor::rotateYaw(ComponentInstance comp, float yaw, bool rads)
+{
+
+}
+
+void TransformComponentProcessor::rotatePitch(ComponentInstance comp, float pitch, bool rads)
+{
+
+}
+
+void TransformComponentProcessor::rotateRoll(ComponentInstance comp, float roll, bool rads)
+{
+
+}
+
+void TransformComponentProcessor::rotateAxis(ComponentInstance comp, float angle, const glm::vec3 &axis, bool rads)
+{
+
+}
+
+glm::vec3 TransformComponentProcessor::getPosition(ComponentInstance comp, bool includeParent)
+{
+    return glm::vec3();
+}
+
+glm::vec3 TransformComponentProcessor::getScale(ComponentInstance comp, bool includeParent)
+{
+    return glm::vec3();
+}
+
+glm::quat TransformComponentProcessor::getOrientation(ComponentInstance comp, bool includeParent)
+{
+    return glm::quat();
+}
+
+glm::mat4 TransformComponentProcessor::getTransformation(ComponentInstance comp)
+{
+    return glm::mat4();
+}
+
+glm::vec3 TransformComponentProcessor::getRotation(ComponentInstance comp, bool rads, bool includeParent)
+{
+    return glm::vec3();
+}
+
+ComponentInstance TransformComponentProcessor::add(Entity e)
+{
+    return ComponentInstance();
+}
+
+void TransformComponentProcessor::remove(Entity e)
+{
+
+}
+
+ComponentInstance TransformComponentProcessor::lookup(Entity e)
+{
+    return ComponentInstance();
+}
+
 }
--- libdf3d/scene/TransformComponentProcessor.h
+++ libdf3d/scene/TransformComponentProcessor.h
@@ -4,11 +4,43 @@
 
 namespace df3d {
 
+// TODO_ecs: rename to scenegraph component mb?
+
 class DF3D_DLL TransformComponentProcessor : utils::NonCopyable
 {
 public:
     TransformComponentProcessor();
     ~TransformComponentProcessor();
+
+    void setPosition(ComponentInstance comp, const glm::vec3 &newPosition);
+    void setPosition(ComponentInstance comp, float x, float y, float z);
+    void setScale(ComponentInstance comp, const glm::vec3 &newScale);
+    void setScale(ComponentInstance comp, float x, float y, float z);
+    void setScale(ComponentInstance comp, float uniform);
+    void setOrientation(ComponentInstance comp, const glm::quat &newOrientation);
+    void setOrientation(ComponentInstance comp, const glm::vec3 &eulerAngles, bool rads = false);
+    void setTransformation(ComponentInstance comp, const glm::mat4 &tr);
+
+    void translate(ComponentInstance comp, const glm::vec3 &v);
+    void translate(ComponentInstance comp, float x, float y, float z);
+    void scale(ComponentInstance comp, const glm::vec3 &v);
+    void scale(ComponentInstance comp, float x, float y, float z);
+    void scale(ComponentInstance comp, float uniform);
+    void rotateYaw(ComponentInstance comp, float yaw, bool rads = false);
+    void rotatePitch(ComponentInstance comp, float pitch, bool rads = false);
+    void rotateRoll(ComponentInstance comp, float roll, bool rads = false);
+    void rotateAxis(ComponentInstance comp, float angle, const glm::vec3 &axis, bool rads = false);
+
+    // TODO_ecs: make getWorldPosition & getLocalPosition instead.
+    glm::vec3 getPosition(ComponentInstance comp, bool includeParent = false);
+    glm::vec3 getScale(ComponentInstance comp, bool includeParent = false);
+    glm::quat getOrientation(ComponentInstance comp, bool includeParent = false);
+    glm::mat4 getTransformation(ComponentInstance comp);
+    glm::vec3 getRotation(ComponentInstance comp, bool rads = false, bool includeParent = false);
+
+    ComponentInstance add(Entity e);
+    void remove(Entity e);
+    ComponentInstance lookup(Entity e);
 };
 
 }
