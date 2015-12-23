5e8b9c619b698ae033fe027ff29a6679ebc02d1b
flamingo <epiforce57@gmail.com>
2015-12-23 21:29:56 +0300

    Fix physics component loader.

--- libdf3d/game/impl/PhysicsComponentLoader.h
+++ libdf3d/game/impl/PhysicsComponentLoader.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <game/EntityComponentLoader.h>
+#include <physics/PhysicsComponentProcessor.h>
+#include <physics/PhysicsComponentCreationParams.h>
 
 namespace df3d {
 
@@ -9,7 +11,10 @@ class PhysicsComponentLoader : public EntityComponentLoader
 public:
     void loadComponent(const Json::Value &root, Entity e, World &w) const override
     {
+        auto params = PhysicsComponentCreationParams(root);
 
+        // NOTE: assuming that mesh already added to this entity.
+        w.physics().add(e, params, w.staticMesh().getMeshData(e));
     }
 };
 
