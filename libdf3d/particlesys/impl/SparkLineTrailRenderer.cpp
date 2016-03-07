0c5e71a163ed27f3f432cd7024677e5b849495bc
flamingo <epiforce57@gmail.com>
2016-03-08 00:20:09 +0300

    Add trail spk renderer.

--- libdf3d/particlesys/impl/SparkLineTrailRenderer.cpp
+++ libdf3d/particlesys/impl/SparkLineTrailRenderer.cpp
@@ -1,6 +1,156 @@
 #include "SparkLineTrailRenderer.h"
 
+#include <libdf3d/render/OpenGLCommon.h>
+#include <libdf3d/base/EngineController.h>
+#include <libdf3d/render/RenderManager.h>
+#include <libdf3d/render/RendererBackend.h>
+#include <libdf3d/render/VertexIndexBuffer.h>
+
 namespace df3d { namespace particlesys_impl {
 
+TrailsParticleSystemRenderer::TrailsParticleSystemRenderer(size_t nbSamples, float duration, float width)
+    : ParticleSystemRenderer(true),
+    m_width(width),
+    m_degeneratedColor(0x00000000)
+{
+    setNbSamples(nbSamples);
+    setDuration(duration);
+}
+
+TrailsParticleSystemRenderer::~TrailsParticleSystemRenderer()
+{
+
+}
+
+void TrailsParticleSystemRenderer::render(const SPK::Group &group, const SPK::DataSet *dataSet, SPK::RenderBuffer *renderBuffer) const
+{
+
+}
+
+void TrailsParticleSystemRenderer::computeAABB(SPK::Vector3D &AABBMin, SPK::Vector3D &AABBMax, const SPK::Group &group, const SPK::DataSet *dataSet) const
+{
+    using namespace SPK;
+
+    const Vector3D* vertexIt = SPK_GET_DATA(const Vector3DArrayData, dataSet, VERTEX_BUFFER_INDEX).getData();
+
+    for (ConstGroupIterator particleIt(group); !particleIt.end(); ++particleIt)
+    {
+        ++vertexIt; // skips pre degenerated vertex
+        for (size_t i = 0; i < m_nbSamples; ++i)
+        {
+            AABBMin.setMin(*vertexIt);
+            AABBMax.setMax(*vertexIt);
+            ++vertexIt;
+        }
+        ++vertexIt; // skips post degenerated vertex
+    }
+}
+
+void TrailsParticleSystemRenderer::init(const SPK::Particle& particle, SPK::DataSet* dataSet) const
+{
+    using namespace SPK;
+
+    size_t index = particle.getIndex();
+    Vector3D* vertexIt = SPK_GET_DATA(Vector3DArrayData, dataSet, VERTEX_BUFFER_INDEX).getParticleData(index);
+    Color* colorIt = SPK_GET_DATA(ColorArrayData, dataSet, COLOR_BUFFER_INDEX).getParticleData(index);
+    float* ageIt = SPK_GET_DATA(FloatArrayData, dataSet, AGE_DATA_INDEX).getParticleData(index);
+    unsigned char* startAlphaIt = SPK_GET_DATA(ArrayData<unsigned char>, dataSet, START_ALPHA_DATA_INDEX).getParticleData(index);
+
+    // Gets the particle's values
+    const Vector3D& pos = particle.position();
+    const Color& color = particle.getColor();
+    float age = particle.getAge();
+
+    // Inits position
+    for (size_t i = 0; i < m_nbSamples + 2; ++i)
+        *(vertexIt++) = pos;
+
+    // Inits color
+    *(colorIt++) = m_degeneratedColor; // degenerate pre vertex
+    for (size_t i = 0; i < m_nbSamples; ++i)
+        *(colorIt++) = color;
+    *colorIt = m_degeneratedColor; // degenerate post vertex
+
+    // Inits age
+    for (size_t i = 0; i < m_nbSamples; ++i)
+        *(ageIt++) = age;
+
+    // Inits start alpha
+    for (size_t i = 0; i < m_nbSamples; ++i)
+        *(startAlphaIt++) = color.a;
+}
+
+void TrailsParticleSystemRenderer::update(const SPK::Group& group, SPK::DataSet* dataSet) const
+{
+    using namespace SPK;
+
+    Vector3D* vertexIt = SPK_GET_DATA(Vector3DArrayData, dataSet, VERTEX_BUFFER_INDEX).getData();
+    Color* colorIt = SPK_GET_DATA(ColorArrayData, dataSet, COLOR_BUFFER_INDEX).getData();
+    float* ageIt = SPK_GET_DATA(FloatArrayData, dataSet, AGE_DATA_INDEX).getData();
+    unsigned char* startAlphaIt = SPK_GET_DATA(ArrayData<unsigned char>, dataSet, START_ALPHA_DATA_INDEX).getData();
+
+    for (ConstGroupIterator particleIt(group); !particleIt.end(); ++particleIt)
+    {
+        const Particle& particle = *particleIt;
+        float age = particle.getAge();
+
+        if (age - *(ageIt + 1) >= m_duration / (m_nbSamples - 1)) // shifts the data by one
+        {
+            std::memmove(vertexIt + 2, vertexIt + 1, (m_nbSamples - 1) * sizeof(Vector3D));
+            std::memmove(colorIt + 2, colorIt + 1, (m_nbSamples - 1) * sizeof(Color));
+            std::memmove(ageIt + 1, ageIt, (m_nbSamples - 1) * sizeof(float));
+            std::memmove(startAlphaIt + 1, startAlphaIt, (m_nbSamples - 1) * sizeof(unsigned char));
+
+            // post degenerated vertex copy
+            std::memcpy(vertexIt + (m_nbSamples + 1), vertexIt + m_nbSamples, sizeof(Vector3D));
+        }
+
+        // Updates the current sample
+        *(vertexIt++) = particle.position();
+        std::memcpy(vertexIt, vertexIt - 1, sizeof(Vector3D));
+        vertexIt += m_nbSamples + 1;
+
+        ++colorIt; // skips post degenerated vertex color
+        *(colorIt++) = particle.getColor();
+        *(startAlphaIt++) = particle.getColor().a;
+
+        *(ageIt++) = age;
+
+        // Updates alpha
+        for (size_t i = 0; i < m_nbSamples - 1; ++i)
+        {
+            float ratio = 1.0f - (age - *(ageIt++)) / m_duration;
+            (colorIt++)->a = static_cast<unsigned char>(*(startAlphaIt++) * (ratio > 0.0f ? ratio : 0.0f));
+        }
+        ++colorIt;
+    }
+}
+
+void TrailsParticleSystemRenderer::createData(SPK::DataSet& dataSet, const SPK::Group& group) const
+{
+    using namespace SPK;
+
+    dataSet.init(NB_DATA);
+    dataSet.setData(VERTEX_BUFFER_INDEX, SPK_NEW(Vector3DArrayData, group.getCapacity(), m_nbSamples + 2));
+    dataSet.setData(COLOR_BUFFER_INDEX, SPK_NEW(ColorArrayData, group.getCapacity(), m_nbSamples + 2));
+    dataSet.setData(AGE_DATA_INDEX, SPK_NEW(FloatArrayData, group.getCapacity(), m_nbSamples));
+    dataSet.setData(START_ALPHA_DATA_INDEX, SPK_NEW(ArrayData<unsigned char>, group.getCapacity(), m_nbSamples));
+
+    // Inits the buffers
+    for (ConstGroupIterator particleIt(group); !particleIt.end(); ++particleIt)
+        init(*particleIt, &dataSet);
+}
+
+void TrailsParticleSystemRenderer::checkData(SPK::DataSet& dataSet, const SPK::Group& group) const
+{
+    using namespace SPK;
+
+    // If the number of samples has changed, we must recreate the buffers
+    if (SPK_GET_DATA(FloatArrayData, &dataSet, AGE_DATA_INDEX).getSizePerParticle() != m_nbSamples)
+    {
+        dataSet.destroyAllData();
+        createData(dataSet, group);
+    }
+}
 
 } }
--- libdf3d/particlesys/impl/SparkLineTrailRenderer.h
+++ libdf3d/particlesys/impl/SparkLineTrailRenderer.h
@@ -1,8 +1,133 @@
 #pragma once
 
+#include "SparkCommon.h"
+#include <libdf3d/render/Vertex.h>
+
 namespace df3d {
 
 namespace particlesys_impl {
 
+class TrailsParticleSystemRenderer : public ParticleSystemRenderer
+{
+    const VertexFormat VERTEX_FORMAT = VertexFormat({ VertexFormat::POSITION_3, VertexFormat::TX_2, VertexFormat::COLOR_4 });
+
+    spark_description(TrailsParticleSystemRenderer, ParticleSystemRenderer)();
+
+private:
+#pragma pack(push, 1)
+    struct SpkVertexData
+    {
+        SPK::Vector3D pos;
+        float tx_u;
+        float tx_v;
+        glm::vec4 color;
+    };
+#pragma pack(pop)
+
+    size_t m_nbSamples;
+    float m_width;
+    float m_duration;
+    SPK::Color m_degeneratedColor;
+
+    static const size_t NB_DATA = 4;
+    static const size_t VERTEX_BUFFER_INDEX = 0;
+    static const size_t COLOR_BUFFER_INDEX = 1;
+    static const size_t AGE_DATA_INDEX = 2;
+    static const size_t START_ALPHA_DATA_INDEX = 3;
+
+    TrailsParticleSystemRenderer(size_t nbSamples = 8, float duration = 1.0f, float width = 1.0f);
+
+public:
+    ~TrailsParticleSystemRenderer();
+
+    /**
+    * @brief Sets the number of samples in a trail
+    *
+    * The number of samples defines the number of points used to construct the trail.<br>
+    * The bigger the number of samples, the smoother the trail but the bigger the compution time and the memory consumption.
+    *
+    * @param nbSamples : the number of samples to construct the trails
+    */
+    void setNbSamples(size_t nbSamples)
+    {
+        assert(nbSamples >= 2 && "GLLineTrailRenderer::setNbSamples(size_t) - The number of samples cannot be less than 2");
+        m_nbSamples = nbSamples;
+    }
+
+    /**
+    * @brief Gets the number of samples per trail
+    * @return the number of samples per trail
+    */
+    size_t getNbSamples() const
+    {
+        return m_nbSamples;
+    }
+
+    /**
+    * @brief Sets the duration of a sample
+    *
+    * The duration of a sample is defined by its life time from its creation to its destruction.<br>
+    * Note that the alpha of a sample will decrease linearly from its initial alpha to 0.
+    *
+    * @param duration : the duration of a sample
+    */
+    void setDuration(float duration)
+    {
+        assert(m_nbSamples > 0.0f && "GLLineTrailRenderer::setDuration(float) - The duration cannot be less or equal to 0.0f");
+        m_duration = duration;
+    }
+
+    /**
+    * @brief Gets the duration of a sample
+    * @return the duration of a sample
+    */
+    float getDuration() const
+    {
+        return m_duration;
+    }
+
+    /**
+    * @brief Sets the width of a trail
+    *
+    * Like for GLLineRenderer, the width is defined in pixels and is not dependant of the distance of the trail from the camera
+    *
+    * @param width : the width of trails in pixels
+    */
+    void setWidth(float width)
+    {
+        m_width = width;
+    }
+
+    /**
+    * @brief Gets the width of a trail
+    * @return the width of a trail (in pixels)
+    */
+    float getWidth() const
+    {
+        return m_width;
+    }
+
+    /**
+    * @brief Sets the color components of degenerated lines
+    * @param color : the color of the degenerated lines
+    */
+    void setDegeneratedLines(SPK::Color color)
+    {
+        m_degeneratedColor = color;
+    }
+
+    void render(const SPK::Group &group, const SPK::DataSet *dataSet, SPK::RenderBuffer *renderBuffer) const override;
+    void computeAABB(SPK::Vector3D &AABBMin, SPK::Vector3D &AABBMax, const SPK::Group &group, const SPK::DataSet *dataSet) const override;
+    void init(const SPK::Particle& particle, SPK::DataSet* dataSet) const override;
+    void update(const SPK::Group& group, SPK::DataSet* dataSet) const override;
+    void createData(SPK::DataSet& dataSet, const SPK::Group& group) const override;
+    void checkData(SPK::DataSet& dataSet, const SPK::Group& group) const override;
+
+    // Creates and registers a new TrailsParticleSystemRenderer.
+    static SPK::Ref<TrailsParticleSystemRenderer> create(size_t nbSamples = 8, float duration = 1.0f, float width = 1.0f)
+    {
+        return SPK_NEW(TrailsParticleSystemRenderer, nbSamples, duration, width);
+    }
+};
 
 } }
