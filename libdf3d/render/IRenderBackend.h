38935616657d4065e727ac21afc89a55a2de96df
flamingo <epiforce57@gmail.com>
2016-03-09 14:36:40 +0300

    Work.

--- libdf3d/render/IRenderBackend.h
+++ libdf3d/render/IRenderBackend.h
@@ -1,9 +1,17 @@
 #pragma once
 
-#include "RenderBackendCommon.h"
+#include "RenderCommon.h"
 #include "Vertex.h"
 
 namespace df3d {
+
+struct RenderBackendCaps
+{
+    float maxAnisotropy;
+};
+
+class PixelBuffer;
+class TextureCreationParams;
 
 class IRenderBackend
 {
@@ -11,20 +19,22 @@ public:
     IRenderBackend() = default;
     virtual ~IRenderBackend() = default;
 
+    virtual const RenderBackendCaps& getCaps() const = 0;
+
     virtual VertexBufferDescriptor createVertexBuffer(const VertexFormat &format, size_t verticesCount, const void *data, GpuBufferUsageType usage) = 0;
     virtual void destroyVertexBuffer(VertexBufferDescriptor vb) = 0;
 
-    virtual void bindVertexBuffer(VertexBufferDescriptor vb, int, int) = 0;
+    virtual void bindVertexBuffer(VertexBufferDescriptor vb, size_t first, size_t count) = 0;
     virtual void updateVertexBuffer(VertexBufferDescriptor vb, size_t verticesCount, const void *data) = 0;
 
     virtual IndexBufferDescriptor createIndexBuffer(size_t indicesCount, const void *data, GpuBufferUsageType usage) = 0;
     virtual void destroyIndexBuffer(IndexBufferDescriptor ib) = 0;
 
-    virtual void bindIndexBuffer(IndexBufferDescriptor ib, int, int) = 0;
+    virtual void bindIndexBuffer(IndexBufferDescriptor ib, size_t first, size_t count) = 0;
     virtual void updateIndexBuffer(IndexBufferDescriptor ib, size_t indicesCount, const void *data) = 0;
 
-    virtual TextureDescriptor createTexture2D() = 0;
-    virtual TextureDescriptor createTextureCube() = 0;
+    virtual TextureDescriptor createTexture2D(const PixelBuffer &pixels, const TextureCreationParams &params) = 0;
+    virtual TextureDescriptor createTextureCube(unique_ptr<PixelBuffer> pixels[(size_t)CubeFace::COUNT], const TextureCreationParams &params) = 0;
     virtual void destroyTexture(TextureDescriptor t) = 0;
     
     virtual void bindTexture(TextureDescriptor t) = 0;
