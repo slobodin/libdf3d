47e1b2aca55577a5c8cabd32c1fda10589d4f5d3
flamingo <epiforce57@gmail.com>
2016-03-15 17:19:02 +0300

    Work.

--- libdf3d/gui/TbManager.cpp
+++ libdf3d/gui/TbManager.cpp
@@ -1,12 +1,31 @@
 #include "TbManager.h"
 
 #include <third-party/turbobadger/src/tb/tb_core.h>
+#include <third-party/turbobadger/src/tb/tb_system.h>
+#include <third-party/turbobadger/src/tb/renderers/tb_renderer_batcher.h>
 #include <third-party/turbobadger/src/tb/tb_bitmap_fragment.h>
 
 namespace tb
 {
 
-class MyImageLoader : public TBImageLoader
+TBImageLoader *TBImageLoader::CreateFromFile(const char *filename)
+{
+    return nullptr;
+}
+
+void TBSystem::RescheduleTimer(double fire_time)
+{
+}
+
+
+}
+
+
+namespace df3d {
+
+namespace gui_impl {
+
+class MyImageLoader : public tb::TBImageLoader
 {
 public:
     /*
@@ -16,24 +35,50 @@ public:
     */
 };
 
-TBImageLoader *TBImageLoader::CreateFromFile(const char *filename)
+class MyRenderer : public tb::TBRendererBatcher
 {
-    return nullptr;
-}
+public:
+    virtual void BeginPaint(int render_target_w, int render_target_h)
+    {
+
+    }
+
+    virtual void EndPaint()
+    {
+
+    }
+
+    virtual tb::TBBitmap *CreateBitmap(int width, int height, tb::uint32 *data)
+    {
+        return nullptr;
+    }
+
+    // == TBRendererBatcher ===============================================================
+
+    virtual void RenderBatch(Batch *batch)
+    {
+
+    }
+
+    virtual void SetClipRect(const tb::TBRect &rect)
+    {
+
+    }
+};
 
 }
 
-
-namespace df3d {
 
 TBManager::TBManager()
 {
+    tb::TBRenderer *r = new gui_impl::MyRenderer();
 
+    tb::tb_core_init(r);
 }
 
 TBManager::~TBManager()
 {
-
+    tb::tb_core_shutdown();
 }
 
 }
