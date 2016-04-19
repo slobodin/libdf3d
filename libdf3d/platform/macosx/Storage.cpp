7823010e9c5ca346c49affeed217973a3119de38
flaming0 <epiforce57@gmail.com>
2016-04-19 21:44:34 +0300

    add mac storage save

--- libdf3d/platform/macosx/Storage.cpp
+++ libdf3d/platform/macosx/Storage.cpp
@@ -6,11 +6,28 @@ class MacosxStorage : public Storage
 {
     void saveToFileSystem(const uint8_t *data, size_t size) override
     {
-
+        std::ofstream of(m_fileName, std::ios::out | std::ios::binary);
+        of.write((const char *)data, size);
     }
 
     bool getFromFileSystem(uint8_t **data, size_t *size) override
     {
+        std::ifstream ifs(m_fileName, std::ios::in | std::ios::binary);
+        if (ifs)
+        {
+            size_t fileSize = 0;
+            ifs.seekg(0, std::ios_base::end);
+            fileSize = ifs.tellg();
+            ifs.seekg(0, std::ios_base::beg);
+
+            *data = new uint8_t[fileSize];
+            *size = fileSize;
+
+            ifs.read((char*)*data, fileSize);
+
+            return true;
+        }
+
         return false;
     }
 
