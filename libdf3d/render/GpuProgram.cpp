09e70b099fbb5a54971e9fab020ebf8a9a8103c5
Nikolay Slobodin <epiforce57@gmail.com>
2016-06-14 14:16:07 +0400

    Update GpuProgram.cpp

--- libdf3d/render/GpuProgram.cpp
+++ libdf3d/render/GpuProgram.cpp
@@ -302,6 +302,8 @@ GpuProgram* GpuProgramManualLoader::load()
 
     auto program = new GpuProgram(gpuProgramDescriptor);
     program->setGUID(m_resourceGuid);
+    
+    need to detach shaders first
 
     // Safely delete the shaders as do not need them.
     svc().renderManager().getBackend().destroyShader(vertexShader);
