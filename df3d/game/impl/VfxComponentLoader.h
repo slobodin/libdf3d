d0ac4d2ff05bd2d36142c49499656fa7153faccd
flamingo <epiforce57@gmail.com>
2016-08-03 23:58:04 +0300

    Minor optimization.

--- df3d/game/impl/VfxComponentLoader.h
+++ df3d/game/impl/VfxComponentLoader.h
@@ -2,15 +2,23 @@
 
 #include <df3d/game/EntityComponentLoader.h>
 #include <df3d/engine/particlesys/ParticleSystemComponentProcessor.h>
+#include <df3d/engine/particlesys/ParticleSystemUtils.h>
 
 namespace df3d {
 
 class VfxComponentLoader : public EntityComponentLoader
 {
+    mutable std::unordered_map<std::string, ParticleSystemCreationParams> m_cache;
+
 public:
     void loadComponent(const Json::Value &root, Entity e, World &w) const override
     {
-        w.vfx().add(e, root["path"].asString());
+        auto pathStr = root["path"].asString();
+        auto found = m_cache.find(pathStr);
+        if (found == m_cache.end())
+            m_cache[pathStr] = ParticleSystemUtils::parseVfx(pathStr);
+
+        w.vfx().add(e, m_cache[pathStr].clone());
     }
 };
 
