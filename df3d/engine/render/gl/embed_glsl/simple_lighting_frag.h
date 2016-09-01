67552996e2c572ab464f5d1932b79017d829736f
flamingo <epiforce57@gmail.com>
2016-09-01 17:31:12 +0300

    Fix.

--- df3d/engine/render/gl/embed_glsl/simple_lighting_frag.h
+++ df3d/engine/render/gl/embed_glsl/simple_lighting_frag.h
@@ -8,6 +8,6 @@ varying LOWP vec2 UV;                       \n\
 void main()                                 \n\
 {                                           \n\
 \
-    gl_FragColor = color * texture2D( diffuseMap, UV ); \n\
+    gl_FragColor = color * texture2D( diffuseMap, UV ); // FIXME: mixing diffuseMap with specular term \n\
 }\
 "
