1cd8abf44c25a588cb1d29d2b86429666fc9ff1e
flamingo <epiforce57@gmail.com>
2016-09-19 16:29:22 +0300

    Work.

---
+++ df3d/platform/windows/InAppPurchase.cpp
@@ -0,0 +1,35 @@
+#include <df3d/platform/InAppPurchase.h>
+
+namespace df3d {
+
+struct InAppPurchase::Impl
+{
+
+};
+
+InAppPurchase::InAppPurchase()
+{
+
+}
+
+InAppPurchase::~InAppPurchase()
+{
+
+}
+
+void InAppPurchase::setDelegate(InAppPurchaseDelegate *delegate)
+{
+
+}
+
+void InAppPurchase::retrieveProductInfo(const std::vector<std::string> &products)
+{
+
+}
+
+void InAppPurchase::purchase(const std::string &productId)
+{
+
+}
+
+}
