37b64358a60ef7c4888e80f9ded5dcb8964e0089
flaming0 <epiforce57@gmail.com>
2016-12-19 20:50:12 +0300

    work

--- df3d/lib/memory/MallocAllocator.cpp
+++ df3d/lib/memory/MallocAllocator.cpp
@@ -1,6 +1,9 @@
 #include "MallocAllocator.h"
 
 #include <df3d/lib/Utils.h>
+#ifndef WIN32
+#include <mm_malloc.h>
+#endif
 
 namespace df3d {
 
@@ -20,7 +23,11 @@ void* MallocAllocator::alloc(size_t size, size_t alignment)
 
     //TODO
     ++m_totalAllocated;
+#ifdef WIN32
     return _aligned_malloc(size, alignment);
+#else
+    return _mm_malloc(size, alignment);
+#endif
 }
 
 void MallocAllocator::dealloc(void *mem)
@@ -33,7 +40,11 @@ void MallocAllocator::dealloc(void *mem)
     --m_totalAllocated;
     DF3D_ASSERT(m_totalAllocated >= 0);
 
+#ifdef WIN32
     _aligned_free(mem);
+#else
+    _mm_free(mem);
+#endif
 }
 
 size_t MallocAllocator::bytesAllocated()
