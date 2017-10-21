ba63372ee837ee469262bb340a1a3f41a069ca05
nslobodin <epiforce57@gmail.com>
2017-10-21 14:43:16 +0300

    Work.

--- tools/atlas_packer/atlas_packer.cpp
+++ tools/atlas_packer/atlas_packer.cpp
@@ -7,17 +7,23 @@
 #include <df3d/df3d.h>
 #include <RectangleBinPack/MaxRectsBinPack.h>
 
-int main(int argc, const char **argv) try
+Json::Value ReadInput(const char *filename)
 {
-    if (argc != 2)
-        throw std::runtime_error("Invalid input. Usage: atlas_packer.exe params.json");
-
-    std::ifstream f(argv[1]);
+    std::ifstream f(filename);
     std::string tmp((std::istreambuf_iterator<char>(f)),
         std::istreambuf_iterator<char>());
     auto jsonRoot = df3d::JsonUtils::fromString(tmp);
     if (jsonRoot.isNull())
         throw std::runtime_error("Atlas Packer: Failed to parse input params");
+    return jsonRoot;
+}
+
+int main(int argc, const char **argv) try
+{
+    if (argc != 3)
+        throw std::runtime_error("Invalid input. Usage: atlas_packer.exe params.json output.json");
+
+    auto jsonRoot = ReadInput(argv[1]);
 
     int width = jsonRoot["size"][0].asInt();
     int height = jsonRoot["size"][1].asInt();
@@ -51,7 +57,12 @@ int main(int argc, const char **argv) try
         result["rects"].append(jsonRect);
     }
 
-    return 0;
+    std::ofstream outf(argv[2]);
+    Json::StreamWriterBuilder b;
+    b["indentation"] = "  ";
+    outf << Json::writeString(b, result);
+
+    return outf.bad() ? 1 : 0;
 }
 catch (std::exception &e)
 {
